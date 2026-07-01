0c6cf0e997dec5eb10370ce7d0b85760edbbe638
Casey Waweru <casey.waweru@gmail.com>
2026-07-01 13:41:48 +0300

    feat: adding and experimenting with dynamic urls

--- server/app.py
+++ server/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return "<p>Welcome to Flatiron Cars</p>"
+
+@app.route('/<string:existing_models>')
+def display_model(existing_models):
+    return f"Flatiron { existing_models } is in out fleet!"
